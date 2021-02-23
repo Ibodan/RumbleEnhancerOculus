3282e304639fcebd25830b050195b439e95ee250
Ibodan <Ibodan@gmail.com>
2021-02-24 06:35:26 +0900

    change default clips

--- RumbleEnhancerOculus/PluginConfig.cs
+++ RumbleEnhancerOculus/PluginConfig.cs
@@ -9,12 +9,12 @@ namespace CustomHapticFeedback
 	{
 		public static PluginConfig Instance { get; set; }
 
-		public string CutClip { get; set; } = "9,9,0,9,9,0,0,6,4,3,3,2,2,2,1,2,1,1,1";
+		public string CutClip { get; set; } = "9,9,0,9,9,0,0,5,4,3,3,2,2,2,1,2,1,1,1";
 		public string MissCutClip { get; set; } = "9,9,9,0,9,9,9,0,9,9,0,9,9,0,5,0,8,0,4,0,7,0,2,0,5,0,4,0,2,0,2,0,1";
 		public string BombClip { get; set; } = "9,9,9,0,9,9,9,0,9,9,0,9,9,0,5,0,8,0,4,0,7,0,2,0,5,0,4,0,2,0,2,0,1";
-		public string UIClip { get; set; } = "2";
-		public string SaberClashClip { get; set; } = "1,1,0,0,1,1,0,5,4,0,0,0,0,9,9,8,0";
-		public string ObstacleClip { get; set; } = "1,1,1,6,8,2,2,3,3,3,5,6";
+		public string UIClip { get; set; } = "1";
+		public string SaberClashClip { get; set; } = "1,1,0,0,7,3,0,9,8,4,0,7,4,0";
+		public string ObstacleClip { get; set; } = "1,2,4,6,7,2,2,3,3,5,7";
 
 		public virtual void Changed()
 		{
